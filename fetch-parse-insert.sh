31a689769d192dc410580b8533ee68ee134bf0e9
Johan Abbors <jabbors@gmail.com>
2026-01-02 12:29:21 +0200

    Correct paths to renamed date files

--- fetch-parse-insert.sh
+++ fetch-parse-insert.sh
@@ -58,7 +58,7 @@ parse_rate() {
 
 last_inserted_time() {
     maturity=$(short_maturity_string $1)
-    file="${DATA_PATH}/euribor-rates-${maturity}.csv"
+    file="${DATA_PATH}/rate-${maturity}.csv"
     tail -n1 ${file} | cut -d ',' -f 1
 }
 
@@ -66,7 +66,7 @@ populate_csv_files() {
     maturity=$(short_maturity_string $1)
     date=$2
     rate=$3
-    file="${DATA_PATH}/euribor-rates-${maturity}.csv"
+    file="${DATA_PATH}/rate-${maturity}.csv"
     echo "append line' ${date},${rate}' to ${file}"
     echo "${date},${rate}" >> ${file}
 }
